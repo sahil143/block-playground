b2ea6f42a83c693d47b69b0a0fbb1d00f8f3d82c
sahil143 <sahilbudhwar143@gmail.com>
2020-06-18 23:08:20 +0530

    initial setup

---
+++ init.sh
@@ -0,0 +1,9 @@
+echo "Initializing Blockchain tools"
+
+
+echo "Installing Dependencies"
+yarn install
+
+echo "=======Creating alias=========="
+alias lexec='PATH=$(npm bin):$PATH'
+echo "=======Aliases created========="
